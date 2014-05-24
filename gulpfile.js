8df8aa874e21c70c00cda586f197cd5100b6374c
Aaron Bell <aaron.bell@gmail.com>
2014-05-24 12:49:38 -0400

    initial setup

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+// optionals
+var bump = require('gulp-bump');
+var git = require('gulp-git');
+
+// update package.json version
+gulp.task('bump-version', function() {
+  return gulp.src('package.json')
+    .pipe(bump())
+    .pipe(gulp.dest('.'));
+});
+
+// commit package.json and push it
+// FIXME push is broken
+gulp.task('release', ['bump-version'], function() {
+  var version = require('./package.json').version;
+
+  return gulp.src('.')
+    .pipe(git.commit('released version ' + version))
+    .pipe(git.push('origin', 'master'))
+    .end();
+});
+
+// merge master into gh-pages and push it
+// FIXME merge fails, seems to happen before checkout is complete
+gulp.task('publish', function() {
+  var version = require('./package.json').version;
+
+  return gulp.src('.')
+    .pipe(git.checkout('gh-pages'))
+    .pipe(git.merge('master'))
+    .pipe(git.commit('merged master ' + version + ' to gh-pages'))
+    .pipe(git.push('origin', 'gh-pages'))
+    .pipe(git.checkout('master'))
+    .end();
+});
